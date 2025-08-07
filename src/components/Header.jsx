16f5e2fc19503fdcf3845ced8432500050f232e1
Lucas Ramalho <adryelll85@gmail.com>
2025-08-07 00:20:45 -0300

    Add: Responsive 1/#

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe, faHeart, faList, faInfoCircle, faCode } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe, faHeart, faList, faInfoCircle, faCode, faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+
     return (
         <header>
             <nav>
                 <div className="brand">
                     <p> <FontAwesomeIcon icon={faCode} /> Home</p>
                 </div>
-                <div className="center-menu-itens">
-                    <p><FontAwesomeIcon icon={faGlobe} /> Countries</p>
-                    <p><FontAwesomeIcon icon={faHeart} /> Favorites</p>
-                    <p><FontAwesomeIcon icon={faList} /> Country List</p>
-                </div>
-                <div className="end-menu-itens">
-                    <p><FontAwesomeIcon icon={faInfoCircle} /> Sobre</p>
+                <button className="menu-toggle" onClick={() => setOpen(!open)}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <div className={`dropdown-menu ${open ? "open" : ""}`}>
+                    <div className="center-menu-itens">
+                        <p><FontAwesomeIcon icon={faGlobe} /> Countries</p>
+                        <p><FontAwesomeIcon icon={faHeart} /> Favorites</p>
+                        <p><FontAwesomeIcon icon={faList} /> Country List</p>
+                    </div>
+                    <div className="end-menu-itens">
+                        <p><FontAwesomeIcon icon={faInfoCircle} /> Sobre</p>
+                    </div>
                 </div>
             </nav>
         </header>
